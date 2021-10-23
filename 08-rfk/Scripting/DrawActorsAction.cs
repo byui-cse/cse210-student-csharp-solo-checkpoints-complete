4d71c9047085d78734c488e3b7df7855363c814d
Scott Burton <burtons@byui.edu>
2021-10-23 17:16:11 -0600

    initial version of rfk

---
+++ 08-rfk/Scripting/DrawActorsAction.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using _08_rfk.Casting;
+using _08_rfk.Services;
+
+
+namespace _08_rfk.Scripting
+{
+    /// <summary>
+    /// An action to draw all of the actors in the game.
+    /// </summary>
+    public class DrawActorsAction : Action
+    {
+        private OutputService _outputService;
+
+        public DrawActorsAction(OutputService outputService)
+        {
+            _outputService = outputService;
+        }
+
+        public override void Execute(Dictionary<string, List<Actor>> cast)
+        {
+            _outputService.StartDrawing();
+
+            foreach (List<Actor> group in cast.Values)
+            {
+                _outputService.DrawActors(group);
+            }
+
+            _outputService.EndDrawing();
+        }
+
+    }
+}
